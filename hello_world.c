e80ba3c13f9222a6ed4a014712e16307a273c40e
Ravi Jain <jravi248@gmail.com>
2021-08-31 13:57:30 +0530

    Update program

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Hello");
+  int n, a[1000];
+  scanf("%d", &n);
+  for (int i=0; i<n; i++) {
+    a[i] = rand()%100;
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+
+  // Even
+  for (int i=0; i<n; i++)
+    if (a[i] % 2 == 0)
+      printf("%d ", a[i]);
+
+  // Odd
+  for (int i=0; i<n; i++)
+    if (a[i] % 2 == 1)
+      printf("%d ", a[i]);
+  printf("\n");
+
   return 0;
 }
